e72de9cbf5b435bd5ac350ed2da89301e2a12a84
Vladislav Aleinikov <v89688941295@gmail.com>
2025-09-03 17:01:40 +0300

    added user & token model

--- server/index.js
+++ server/index.js
@@ -3,12 +3,18 @@ require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT;
 const app = express()
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
 const start = async () => {
     try {
+        await mongoose.connect(process.env.DB_URL)
         app.listen(PORT, ()=> console.log('Server running on port:', PORT));
     } catch (e) {
         console.error(e);
