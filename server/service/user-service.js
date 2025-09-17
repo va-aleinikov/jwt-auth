544d69b74c52ea4cbf11e498306e721e9aa14561
Vladislav Aleinikov <v89688941295@gmail.com>
2025-09-17 12:32:29 +0300

    added activation for users

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -22,6 +22,15 @@ class UserService {
 
         return {...tokens, user: userDto};
     }
+
+    async activate(activationLink) {
+        const user = await UserModel.findOne({activationLink})
+        if (!user) {
+            throw new Error('user not found');
+        }
+        user.isActivated = true;
+        await user.save();
+    }
 }
 
 module.exports = new UserService();
